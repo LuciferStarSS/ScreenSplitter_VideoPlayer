26c1053c33f7614a4b1167acebd2c0943ac0f0aa
L.S <79638053+LuciferStarSS@users.noreply.github.com>
2022-05-21 16:21:18 +0800

    Add files via upload
    单个节目的完整配置数据

---
+++ data1/data.inc.php
@@ -0,0 +1,10 @@
+<?php
+$vname=Array("吴老师","学生一","学生二","学生端");				//对应的视频用户
+$vfile=Array("./data1/1.mp4","./data1/2.mp4","./data1/3.mp4","./data1/4.mp4");	//对应的视频文件
+$vstart_time=Array(0,6.99,8.19,7.42);
+$video_height=686;								//视频高
+$video_width=1050;								//视频宽
+$stfile="./data1/subtitle.txt";							//字幕
+
+$vcount=count($vfile);								//视频总数
+?>
