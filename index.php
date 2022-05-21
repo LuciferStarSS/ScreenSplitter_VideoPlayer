c3d8bb826fe2e9c11a0ff0dd0471e0e580d96964
L.S <79638053+LuciferStarSS@users.noreply.github.com>
2022-05-21 16:20:22 +0800

    Update index.php
    添加：
    彻底剥离节目单数据
    语音播放指示
    自动切屏

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php
+$f="./data1/";
 
-$vcount=4;						//视频总数
-$vname=Array("吴老师","学生一","学生二","学生三");	//对应的视频用户
-$vfile=Array("./data1/1.mp4","./data1/2.mp4","./data1/3.mp4","./data1/4.mp4");		//对应的视频文件
-$stfile="./data1/subtitle.txt";
-$video_height=642;					//视频高
-$video_width=1024;					//视频宽
+include($f."data.inc.php");
 
 $video_s_height=floor($video_height/($vcount-1)-1);			//小视窗高    小视窗数量为n=($vcount-1);
 $video_s_width=round($video_s_height*($video_width/$video_height));	//小视窗宽
@@ -35,8 +31,21 @@ for($i=0;$i<$vcount;$i++)			//初始化视频和DIV的句柄
 {
    echo "var d".$i."=null;\r\n";			//DIV
    echo "var v".$i."=null;\r\n";			//VIDEO
+   echo "var p".$i."=null;\r\n";			//PLAYSTATUS
 }
 ?>
+
+//音频播放指示器静音
+function muteall()
+{
+<?php 
+for($i=0;$i<$vcount;$i++)
+{
+   echo "   p".$i.".style.display=\"none\";\r\n";
+}
+?>
+}
+
 
 //全体暂停
 function pause()
@@ -71,6 +80,7 @@ for($i=0;$i<$vcount;$i++)		//遍历所有视频
 {
    echo "   d".$i."=document.getElementById(\"d".$i."\");\r\n";
    echo "   v".$i."=document.getElementById(\"v".$i."\");\r\n";
+   echo "   p".$i."=document.getElementById(\"p".$i."\");\r\n";
    echo "   d".$i.".style.display=\"block\";\r\n";
    echo "   v".$i.".style.display=\"block\";\r\n";
    if($i==0)								//主视频
@@ -80,9 +90,9 @@ for($i=0;$i<$vcount;$i++)		//遍历所有视频
       echo "   d".$i.".style.left=\"0px\";\r\n";
       echo "   d".$i.".style.top=\"0px\";\r\n";
       echo "   d".$i.".style.width=\"".$video_width."px\";\r\n";
-      echo "   d".$i.".style.height=\"".$video_height."px\";\r\n";
+      echo "   d".$i.".style.height=\"".($video_height+1)."px\";\r\n";
       echo "   d".$i.".style.borderColor=\"red\";\r\n";
-      echo "   posArr[".$i."]=Array(".($video_width).",".($video_height).",0,0);\r\n";	//主视频：{宽width，高height，左偏移量left，顶部偏移量top}
+      echo "   posArr[".$i."]=Array(".($video_width).",".($video_height+1).",0,0);\r\n";	//主视频：{宽width，高height，左偏移量left，顶部偏移量top}
    }
    else									//分视频
    {
@@ -91,7 +101,7 @@ for($i=0;$i<$vcount;$i++)		//遍历所有视频
       echo "   d".$i.".style.left=\"".($video_width+6)."px\";\r\n";
       echo "   d".$i.".style.top=\"".(($i-1)*($video_s_height+2))."px\";\r\n";
       echo "   d".$i.".style.width=\"".($video_s_width)."px\";\r\n";
-      echo "   d".$i.".style.height=\"".($video_s_height-1)."px\";\r\n";
+      echo "   d".$i.".style.height=\"".($video_s_height)."px\";\r\n";
       echo "   d".$i.".style.borderColor=\"gray\";\r\n";
       echo "   posArr[".$i."]=Array(".($video_s_width).",".($video_s_height).",".($video_width).",".(($i-1)*($video_s_height+2)).");\r\n";//分视频：{宽width，高height，左偏移量left，顶部偏移量top}
    }
@@ -100,7 +110,6 @@ for($i=0;$i<$vcount;$i++)		//遍历所有视频
    currentMainDiv=d0;						//默认第一个视频在主屏位置
    comments=document.getElementById("comments");
    comments.innerHTML="本视频演示平台中的小组分享功能。";	//初始化字幕
-
 }
 
 //响应视频点击切换主副屏操作
@@ -114,8 +123,8 @@ function setme(obj)
       {
          $("#d"+j).css("zIndex",9999);				//主屏置顶
 
-         $("#v"+j).animate({"width":(posArr[0][0]),"height":(posArr[0][1]),"left":0,"top":-26},3000);	//在Chrome里，top为-26；在IE里，top的偏移量为-23.
-         $("#d"+j).animate({"width":posArr[0][0],"height":posArr[0][1],"left":0,"top":0},3000);
+         $("#v"+j).animate({"width":(posArr[0][0]),"height":(posArr[0][1]+1),"left":0,"top":-27},1000);	//在Chrome里，top为-26；在IE里，top的偏移量为-23.
+         $("#d"+j).animate({"width":posArr[0][0],"height":(posArr[0][1]),"left":0,"top":0},1000);
          $("#d"+j).css("borderColor","red");
       }
       else//其它分屏
@@ -127,8 +136,8 @@ function setme(obj)
          else
             $("#d"+j).css("zIndex",99);
 
-         $("#v"+j).animate({"width":(posArr[i][0]),"height":(posArr[i][1]),"left":0,"top":-26},3000);					//在Chrome里，top为-26；在IE里，top的偏移量为-23.
-         $("#d"+j).animate({"width":(posArr[i][0]),"height":(posArr[i][1]-1),"left":(posArr[i][2]+6),"top":(posArr[i][3])},3000);
+         $("#v"+j).animate({"width":(posArr[i][0]),"height":(posArr[i][1]+1),"left":0,"top":-27},1000);					//在Chrome里，top为-27；在IE里，top的偏移量为-23.
+         $("#d"+j).animate({"width":(posArr[i][0]),"height":(posArr[i][1]),"left":(posArr[i][2]+6),"top":(posArr[i][3])},1000);
          $("#d"+j).css("borderColor","gray");
       }
    }
@@ -142,26 +151,31 @@ function go()
    v0.ontimeupdate= function(event){onTrackedVideoFrame(this.currentTime, this.duration);};
 }
 
+<?php 
+for($i=1;$i<$vcount;$i++)
+{
+   echo "var v".$i."_play=false;\r\n";
+}
+?>
 //分视频和字幕的控制
+var speaker=null;
 function onTrackedVideoFrame(currentTime, duration){
-   if(currentTime>6.99 && currentTime <7.12)//时序
-   {
-      v1_play=1;
-      v1.play();
-   }
-   else if(currentTime>8.19&& currentTime <8.21)
-   {
-      v2_play=1;
-      v2.play();
-   }
-   else if(currentTime>7.42&& currentTime <7.47)
-   {
-      v3_play=1;
-      v3.play();
-   }
 
+<?php 
+for($i=1;$i<$vcount;$i++)//分屏视频开始的时间点
+{
+   if($i>1) echo "   else";
+
+   echo "   if(currentTime>".$vstart_time[$i]." && v".$i."_play == false)//时序
+   {
+      v".$i."_play=true;
+      v".$i.".play();
+   }
+   ";
+}
+?>
 <?php
-$subtitle=@file_get_contents($stfile);
+$subtitle=@file_get_contents($stfile);//字幕加载控制
 if($subtitle!="")
 {
    $subArrs=explode("\r\n",trim($subtitle,"\r\n"));
@@ -172,12 +186,22 @@ if($subtitle!="")
       $subdataArr=explode("\t",$subArrs[$i]);
       if(count($subdataArr)==3)
       {
+
+//输出JS脚本
 ?>
-   else if(currentTime><?php echo $subdataArr[0];?> && currentTime <<?php echo ($subdataArr[0]+0.3);?>)
+   else   if(currentTime > <?php echo $subdataArr[0];?> && currentTime < <?php echo ($subdataArr[0]+0.3);?>)
    {
-      comments.innerHTML="<?php echo $subdataArr[1]."".$subdataArr[2];?>";
+      comments.innerHTML="<?php echo "[".trim($subdataArr[1])."]".$subdataArr[2];?>";
+      muteall();
 <?php
-         if($i==$sc-1)
+//增加语音播放指示
+      $u=array_search($subdataArr[1],$vname);
+      if($u!==false)
+      {
+         echo "      p".$u.".style.display=\"inline\";\r\n";
+         echo "      if(speaker!=p".$u."){ speaker=p".$u."; setme(v".$u.");}\r\n";
+      }
+         if($i==$sc-1)//结尾淡出效果
          {
             for($j=0;$j<$vcount;$j++)
             {
@@ -202,8 +226,8 @@ for($i=0;$i<$vcount;$i++)
 {
 ?>
     <div id="d<?php echo $i;?>" onclick=setme(this) style="display:none;position:absolute;left:0px;top:0px;border:solid;border-color:gray">
-      <div style="position:relative;left:12px;top:11px;z-index:9;background:snow;font-size:20px;width:100px;text-align:center;"><?php echo $vname[$i];?></div>
-      <video  id="v<?php echo $i;?>" class=minv  src="<?php echo $vfile[$i];?>" autoplay="false" autostart=false style="display:none;position:relative;left:0px;top:-26px;">
+      <div style="position:relative;left:12px;top:11px;z-index:9;background:snow;font-size:20px;width:100px;text-align:center;"><?php echo $vname[$i];?><img id="p<?php echo $i;?>" style="display:none;position:absolute;top:3px;" src=./img/play.gif height=20></div>
+      <video  id="v<?php echo $i;?>" class=minv  src="<?php echo $vfile[$i];?>" autoplay="false" autostart=false style="display:none;position:relative;left:0px;top:-27px;">
         <param name="autostart" value="false"/>
         <param src="<?php echo $vfile[$i];?>"/>
       </video>
