fab09d0944177c8296e445e0382b37b2540eb0ef
L.S <79638053+LuciferStarSS@users.noreply.github.com>
2022-05-26 22:04:45 +0800

    Update data.inc.php
    添加音乐播放控制。

--- data1/data.inc.php
+++ data1/data.inc.php
@@ -1,9 +1,10 @@
 <?php
 $vname=Array("吴老师","学生一","学生二","学生端");				//对应的视频用户
-$vfile=Array("./data1/1.mp4","./data1/2.mp4","./data1/3.mp4","./data1/4.mp4");	//对应的视频文件
-$vstart_time=Array(0,6.99,8.19,7.42);
-$video_height=686;								//视频高
-$video_width=1050;								//视频宽
+$vfile=Array("./data1/1/1.mp4","./data1/2/2.mp4","./data1/3/3.mp4","./data1/4/4.mp4");	//对应的视频文件
+//$afile="./data1/audio.mp3";                           //如果视频没有音轨数据，则可以用这个来播放背景音乐。
+$vstart_time=Array(0.01,0.01,0.01,0.01);
+$video_height=642;								//视频高
+$video_width=1024;								//视频宽
 $stfile="./data1/subtitle.txt";							//字幕
 
 $vcount=count($vfile);								//视频总数
